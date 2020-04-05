1e5a2e547d325f5290a2a52e1552fe3bd960018a
shoaibgeeksquad <shoaib.geeksquad@gmail.com>
2020-04-06 00:46:43 +0500

    style sheet guide created for forms

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import React from 'react';
+import './Assets/Style/formStyle.css'
+import './Assets/Style/commonStyle.css'
+import Header from './Components/Header/header'
 
 export default () => {
   return (
     <>
-      <h1>Starting Riverside Insights New Project</h1>
+        <Header/>
     </>
   );
 }
