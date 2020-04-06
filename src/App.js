c1298b34737ed6034fa900d03aeb06ef47e002da
adeelgeeksquad <development.geeksquad@gmail.com>
2020-04-06 15:04:17 +0500

    pool component

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import './Assets/Style/style.css'
 import Header from './Components/Header/header'
-// import StartSimulation from './Components/StartSimulation/startSimulation'
+import StartSimulation from './Components/StartSimulation/startSimulation'
 import Signin from './Container/Signin/index'
-
+import Pool from './Components/Pool/pool'
 export default () => {
   return (
     <>
         <Header/>
-        <Signin/>
+        <Pool/>
     </>
   );
 }
